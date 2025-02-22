2be20a3a72c0b25e9388b3a824c05afab7dcdf19
lieroes <lars-betting@protonmail.com>
2025-02-22 21:22:42 +0100

    Finished chapter 6

--- src/script.js
+++ src/script.js
@@ -1,12 +1,27 @@
 import './style.css'
 import * as THREE from 'three'
 import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+    console.log(event.clientX, event.clientY)
+})
+
 
 /**
  * Object
@@ -33,14 +48,16 @@ const sizes = {
 /**
  * Camera
  */
-//const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+//controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 /**
  * Renderer
  */
@@ -54,9 +71,20 @@ const clock = new THREE.Clock()
 const tick  = () => {
     
     const elapsedTime = clock.getElapsedTime()
-    mesh.rotation.y = elapsedTime
-   renderer.render(scene, camera)
-   window.requestAnimationFrame(tick)
+
+    //update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 6
+    // camera.lookAt(mesh.position)
+    //mesh.rotation.y = elapsedTime
+
+    //update controls
+    controls.update()
+
+
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
    
 
 }
