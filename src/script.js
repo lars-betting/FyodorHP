49a8073cf541d0f347f3703f69d28436a742930f
lieroes <lars-betting@protonmail.com>
2025-02-24 13:15:46 +0100

    finished lesson on textures

--- src/script.js
+++ src/script.js
@@ -1,143 +1,102 @@
-import './style.css'
 import * as THREE from 'three'
-import gsap from 'gsap'
+import './style.css'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * cursor
-*/
-const cursor = {
-    x: 0,
-    y: 0
-}
-window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX / sizes.width - 0.5
-    cursor.y = -(event.clientY / sizes.height - 0.5)
-    //console.log(event.clientX, event.clientY)
-})
+//axeshelper
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
 
-// const geometry = new THREE.BufferGeometry()
-
-// const count = 500
-// const positionsArray = new Float32Array(count * 3 * 3)
-// for(let i = 0; i < count * 3 * 3; i++) {
-//     positionsArray[i] = (Math.random() - 0.5) * 4
-// }
-// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
-// geometry.setAttribute('position', positionsAttribute)
-
-// const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
-// const mesh = new THREE.Mesh(geometry, material)
-// scene.add(mesh)
 /**
  * Object
-*/
+ */
+const loadingManager = new THREE.LoadingManager()
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const texture = textureLoader.load('/textures/checkerboard-1024x1024.png')
 
-// const mesh = new THREE.Mesh(
-    //     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    //     new THREE.MeshBasicMaterial({ color: 0xff0000 })
-    // )
-    // scene.add(mesh)
-    
-    //axes helper
-    const loader = new GLTFLoader()
-    const fyodorLogo = new THREE.Object3D()
-    const fyodorLogoLoader = new GLTFLoader()
-    fyodorLogoLoader.load(
-        'fyodor.glb',
-        (gltf) => {
-            const box = new THREE.Box3().setFromObject(gltf.scene)
-            const size = box.getSize(new THREE.Vector3())
-            const scale = 1 / size.length() * 2
-            
-            gltf.scene.scale.set(scale, scale, scale)
-            fyodorLogo.add(gltf.scene)
-            scene.add(fyodorLogo)
-        }
-    )
-    
-    
-    const axesHelper = new THREE.AxesHelper(3);
-    scene.add(axesHelper)
-    /**
-     * Sizes
-    */
-   const sizes = {
-       width: window.innerWidth,
-       height: window.innerHeight
-    }
-    window.addEventListener('resize', () => 
-    {
-        sizes.width = window.innerWidth
-        sizes.height = window.innerHeight
+texture.wrapS = THREE.RepeatWrapping
+texture.wrapT = THREE.RepeatWrapping
 
-        camera.aspect = sizes.width / sizes.height
-        camera.updateProjectionMatrix()
-        renderer.setSize(sizes.width, sizes.height)
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio),2)
-    })
-    window.addEventListener('keypress', (event) => {
-    if (event.key === 'f') {
-        if(document.fullscreenElement) { //might not work on safari
-            document.exitFullscreen()
-        }
-        else {
-            canvas.requestFullscreen()
-        }
-        
-    }})
-    
-    /**
-     * Camera
-    */
-   const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-   // camera.position.x = 2
-// camera.position.y = 2
-camera.position.z = 3
-//camera.lookAt(mesh.position)
+texture.generateMipmaps = false;
+texture.minFilter = THREE.NearestFilter //checkout substance designer
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ map: texture })
+//const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 scene.add(camera)
 
-//controls
+// Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio),2)
-renderer.setClearColor( 0xffff99, 1)
-const clock = new THREE.Clock()
-//Animation
-const tick  = () => {
-    
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
     const elapsedTime = clock.getElapsedTime()
 
-    //update camera
-    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    // camera.position.y = cursor.y * 6
-    // camera.lookAt(mesh.position)
-    //mesh.rotation.y = elapsedTime
-    //fyodorLogo.rotation.y = elapsedTime
-    //update controls
+    // Update controls
     controls.update()
 
-    
+    // Render
     renderer.render(scene, camera)
-    window.requestAnimationFrame(tick)
-   
 
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
 tick()
