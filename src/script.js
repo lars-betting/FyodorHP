c011367b0b0816d3f7fb510466be53b6593ec913
lieroes <lars-betting@protonmail.com>
2025-02-26 09:32:26 +0100

    new src and static folders for light project

--- src/script.js
+++ src/script.js
@@ -1,74 +1,64 @@
 import * as THREE from 'three'
-import './style.css'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
 
 /**
  * Base
  */
+// Debug
+const gui = new GUI()
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-//textures
-const textureLoader = new THREE.TextureLoader()
-//const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
-const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
-const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-//const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
-const gradientTexture = textureLoader.load('/textures/gradients/5.jpg')
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.5)
+scene.add(ambientLight)
 
-//doorColorTexture.colorSpace = THREE.sRGBEncoding
-//matcapTexture.colorSpace = THREE.sRGBEncoding
-
-//axeshelper
-const axesHelper = new THREE.AxesHelper(3)
-scene.add(axesHelper)
+const pointLight = new THREE.PointLight(0xffffff, 50)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 /**
- * Object
+ * Objects
  */
-const doorColorTexture = textureLoader.load(
-    '/textures/door/color.jpg',
-    () => {
-        console.log('Texture loaded successfully');
-        doorColorTexture.colorSpace = THREE.SRGBColorSpace;
-    })
-const matcapTexture = textureLoader.load(
-    '/textures/matcaps/8.png',
-    () => {
-        console.log('Texture loaded successfully');
-        matcapTexture.colorSpace = THREE.SRGBColorSpace;
-    })
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
 
-const material = new THREE.MeshBasicMaterial()
-material.map = doorColorTexture
-
-const sphereMesh = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
+// Objects
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
     material
 )
-const planeMesh = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1),
-    material
-)
-const torusMesh = new THREE.Mesh(
-    new THREE.TorusGeometry(0.6, 0.2, 16, 32),
+sphere.position.x = - 1.5
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
     material
 )
 
-//transforms
-sphereMesh.position.x = -1.5
-planeMesh.position.x = 0
-torusMesh.position.x = 1.5
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+)
+torus.position.x = 1.5
 
-//add to scene
-scene.add(sphereMesh, planeMesh, torusMesh)
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(sphere, cube, torus, plane)
 
 /**
  * Sizes
@@ -125,20 +115,20 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    cube.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    cube.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
+
     // Update controls
     controls.update()
 
     // Render
     renderer.render(scene, camera)
-
-    //rotation
-    sphereMesh.rotation.x = 0.15* elapsedTime
-    planeMesh.rotation.x = 0.15* elapsedTime
-    torusMesh.rotation.x = 0.15* elapsedTime
-
-    sphereMesh.rotation.y = 0.1* elapsedTime
-    planeMesh.rotation.y = 0.1* elapsedTime
-    torusMesh.rotation.y = 0.1* elapsedTime
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
